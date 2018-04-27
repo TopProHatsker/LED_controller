e77d66a3451fb5a3829ec00a21b29395c7c3dea6
Artem <37265694+TopProHatsker@users.noreply.github.com>
2018-04-27 20:21:42 +0300

    Add files via upload

---
+++ Debug.ino
@@ -0,0 +1,326 @@
+
+void Debg(){
+  
+  while(1){ 
+    str = Serial.readString();    //чтение комманды  
+
+    if (str != 0){
+      Serial.println (str);
+      comm();
+    }
+  }
+
+}
+
+
+void comm(){
+    
+    if (str == "help" || str == "/help"){
+        Serial.println ("dir; cd_free; new; del; edit; change; reduce; format;");
+        Serial.println ("mem_format; mem_write; mem_read; light_ON; light_OFF;");
+        Serial.println ("reboot, restart, reset;");
+    }
+    
+    
+    else if (str == "LED")
+        Serial.println (LED);
+    
+    
+    else if (str == "light_ON")
+        lightOn();
+    
+    else if (str == "light_OFF")
+        lightOff();
+    
+    
+  
+    else if (str == "dir")
+        dir();
+
+    else if (str == "cd_free")
+        cd_free();
+
+    else if (str == "new")
+        New();
+
+    else if (str == "del")
+        del();
+    
+    else if (str == "edit")
+        edit();
+    
+    else if (str == "change")
+        change();
+    
+    else if (str == "format")
+        format();
+  
+    else if (str == "reduce")
+        reduce();
+   
+    
+   
+    else if (str == "mem_format")
+        mem_format();
+    
+    else if (str == "mem_write")
+        mem_Write();
+    
+    else if (str == "mem_read")
+        mem_Read();
+    
+    else if (str == "mem_dir")
+        mem_dir();
+   
+    
+    else if (str == "reboot" || str == "reset" || str == "restart"){
+       for (int a=0; a<3; a++){ Serial.print ("."); delay (500);}
+       void(* resetFunc) (void) = 0; 
+       resetFunc();
+    }
+    
+    else
+        Serial.println ("no command!");
+}
+
+
+
+
+
+void cd_free(){
+    for (i=0; i<cells; i++){
+          if (buff[i][0]==0 && buff[i][1]==0 && buff[i][2]==0 && buff[i][3]==0 && buff[i][4]==0){
+           Serial.print (" free: "); Serial.println (i);
+          } 
+    }
+}
+
+
+void New(){   
+        cd_free();
+        Serial.print ("\ncell: ");   i = intIn();        
+        Serial.print (" R = ");      buff[i][0] = intIn();        
+        Serial.print (" G = ");      buff[i][1] = intIn();        
+        Serial.print (" B = ");      buff[i][2] = intIn();        
+        Serial.print (" light = ");  buff[i][3] = intIn();
+        Serial.print (" mode: ");    buff[i][4] = intIn();
+        
+   Serial.println ("...");
+}
+
+
+void del(){
+
+       Serial.print (" cell: ");  i = intIn(); 
+        buff[i][0]=0;
+        buff[i][1]=0;
+        buff[i][2]=0;
+        buff[i][3]=0;
+        buff[i][4]=0;
+       Serial.println ("...");
+}
+
+
+void edit(){
+   
+    Serial.print (" cell: ");   i = intIn();    
+    Serial.println ("\tR\tB\tG\tLight\tMode ");
+  
+    Serial.print (" old = ");
+     Serial.print (buff[i][0]); Serial.print ("\t");
+     Serial.print (buff[i][1]); Serial.print ("\t");
+     Serial.print (buff[i][2]); Serial.print ("\t");
+     Serial.print (buff[i][3]); Serial.print ("\t");
+     Serial.print (buff[i][4]); Serial.print ("\n\n");
+
+    Serial.println (" new: ");
+     Serial.print ("  R = ");     buff[i][0] = intIn();     
+     Serial.print ("  G = ");     buff[i][1] = intIn();     
+     Serial.print ("  B = ");     buff[i][2] = intIn();     
+     Serial.print ("  light = "); buff[i][3] = intIn(); 
+     Serial.print ("  mode: ");   buff[i][4] = intIn();
+     
+  Serial.println ("...");
+}
+
+
+void change(){
+    Serial.print (" cell_1: "); 
+    i = intIn();
+    
+     x[0] = buff[i][0];
+     x[1] = buff[i][1];
+     x[2] = buff[i][2];
+     x[3] = buff[i][3];
+     x[4] = buff[i][4];
+     
+    Serial.print (" cell_2: "); 
+    j = intIn();
+   
+     buff[i][0]=buff[j][0];
+     buff[i][1]=buff[j][1];
+     buff[i][2]=buff[j][2];
+     buff[i][3]=buff[j][3];
+     buff[i][4]=buff[j][4];
+
+     buff[j][0]=x[0];
+     buff[j][1]=x[1];
+     buff[j][2]=x[2];
+     buff[j][3]=x[3];
+     buff[j][3]=x[4];
+     
+    Serial.println ("...");  
+} 
+
+
+void dir(){
+    Serial.println ("\t   R\t B\t G     Light\tMode");
+    for (i=0; i<cells; i++){
+        Serial.print (" cell_"); Serial.print (i); Serial.print (" = ");
+        Serial.print (buff[i][0]); Serial.print ("\t");
+        Serial.print (buff[i][1]); Serial.print ("\t");
+        Serial.print (buff[i][2]); Serial.print ("\t");
+        Serial.print (buff[i][3]); Serial.print ("\t");
+        Serial.print (buff[i][4]); Serial.print ("\n");
+    }
+}
+
+
+void format(){
+  for (i=0; i<cells; i++){
+    buff[i][0]=0;
+    buff[i][1]=0;
+    buff[i][2]=0;
+    buff[i][3]=0;
+    buff[i][4]=0;
+  }  
+Serial.println ("...");
+}
+
+
+void reduce(){
+
+    for (i=0; i<cells; i++){
+        if (buff[i][0]==0 && buff[i][1]==0 && buff[i][2]==0 && buff[i][3]==0 && buff[i][4]==0){
+
+          j=i;
+          for (j; j<cells; j++){
+            if (buff[j][0]!=0 || buff[j][1]!=0 || buff[j][2]!=0 || buff[j][3]!=0 || buff[j][4]!=0){
+
+              buff[i][0]=buff[j][0];
+              buff[i][1]=buff[j][1];
+              buff[i][2]=buff[j][2];
+              buff[i][3]=buff[j][3];
+              buff[i][4]=buff[j][4];
+
+              buff[j][0]=0;
+              buff[j][1]=0;
+              buff[j][2]=0;
+              buff[j][3]=0;
+              buff[j][4]=0;
+
+              break;
+            }
+          }
+        }
+    }
+  Serial.println ("...");
+}
+
+
+
+//--------------------------EEPROM-------------------------------
+
+void mem_Read(){
+
+  for (i=0; i<cells; i++){
+    buff[i][0]=EEPROM.read(i*5+0); 
+    buff[i][1]=EEPROM.read(i*5+1);
+    buff[i][2]=EEPROM.read(i*5+2);
+    buff[i][3]=EEPROM.read(i*5+3);
+    buff[i][4]=EEPROM.read(i*5+4);
+  }
+ Serial.println ("...");
+}
+
+
+void mem_Write(){
+  for (i=0; i<cells; i++){ 
+    if (buff[i][0] != EEPROM.read(i*5+0))  EEPROM.write(i*5+0, buff[i][0]);    
+    if (buff[i][1] != EEPROM.read(i*5+1))  EEPROM.write(i*5+1, buff[i][1]);     
+    if (buff[i][2] != EEPROM.read(i*5+2))  EEPROM.write(i*5+2, buff[i][2]);     
+    if (buff[i][3] != EEPROM.read(i*5+3))  EEPROM.write(i*5+3, buff[i][3]);
+    if (buff[i][3] != EEPROM.read(i*5+4))  EEPROM.write(i*5+4, buff[i][4]);      
+  }
+  Serial.println ("...");
+}
+
+
+void mem_dir(){
+  for (i=0; i<cells; i++){
+     Serial.print ("\t");   Serial.print (EEPROM.read(i*5+0));      
+     Serial.print ("\t");   Serial.print (EEPROM.read(i*5+1));                 
+     Serial.print ("\t");   Serial.print (EEPROM.read(i*5+2));       
+     Serial.print ("\t");   Serial.print (EEPROM.read(i*5+3));  
+     Serial.print ("\t");   Serial.print (EEPROM.read(i*5+4)); 
+        
+     Serial.print ("\n");
+  }
+}
+
+
+void mem_format(){
+  for (i=0; i<cells; i++){
+    if (0 != EEPROM.read(i*5+0))  EEPROM.write(i*5+0, 0);    
+    if (0 != EEPROM.read(i*5+1))  EEPROM.write(i*5+1, 0);   
+    if (0 != EEPROM.read(i*5+2))  EEPROM.write(i*5+2, 0);       
+    if (0 != EEPROM.read(i*5+3))  EEPROM.write(i*5+3, 0);
+    if (0 != EEPROM.read(i*5+4))  EEPROM.write(i*5+4, 0);      
+  }
+  Serial.println ("...");
+}
+//--------------------------EEPROM-------------------------------
+
+
+
+
+//---------------------------LED-------------------------------
+
+void lightOn(){
+  Serial.print (" !!![ using mode=0 ]!!! ");
+  Serial.print (" sell: ");
+  
+   i = intIn(); 
+   light = map(buff[i][3], 0, 255, 0, 100);
+   light /= 100;
+  
+  analogWrite (R,buff[i][0]*light); 
+  analogWrite (G,buff[i][1]*light);
+  analogWrite (B,buff[i][2]*light);  
+
+}
+
+
+void lightOff(){
+  analogWrite (R,0);
+  analogWrite (G,0);
+  analogWrite (B,0);
+  Serial.println ("...");
+}
+//---------------------------LED-------------------------------
+
+
+
+
+int intIn(){                                      //убрать костыль!!!!!!!!!!!!
+    
+    if (Serial.available() > 0){
+      val = Serial.parseInt();
+      Serial.println(val);
+      return val;
+    }
+
+intIn();    
+}  
+
+
---
+++ LED_PC_6.2.ino
@@ -0,0 +1,255 @@
+//Created by Artem Karchenko (2018 02 11)
+
+
+
+
+//===========================================
+//-----------ИСПРАВИТЬ ДОБАВИТЬ--------------
+// СТРОКА 65 минимальное время меньше
+// ВО ВСЕХ МОДАХ УБРАТЬ delay;
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//===========================================
+
+
+
+
+
+#include <EEPROM.h> // подключаем библиотеку EEPROM
+
+
+//------------------settings-----------------
+    double version = 6.40;
+    short const cells = 10;
+    #define R 9
+    #define G 10
+    #define B 11
+//------------------settings-----------------
+
+ String str;
+ float  light=0, real_light=0, a=0, b=0;
+ byte i=0, j=0, val=0; 
+ 
+ boolean prev_LED=0, LED=0, up=0;
+ short int num=0;
+ unsigned long time=0;
+ 
+ short int buff[cells][5], x[5];
+
+
+byte R_val, G_val, B_val;
+byte prev_R_val, prev_G_val, prev_B_val;
+
+
+void setup(){
+  
+  pinMode (2, INPUT_PULLUP);
+  pinMode (R, OUTPUT);
+  pinMode (G, OUTPUT);
+  pinMode (B, OUTPUT);
+  
+   delay (2000);
+   if (digitalRead(2) == 0){
+     Serial.begin(9600);
+      Serial.println ("debug mode:"); 
+      Serial.print ("version: "); 
+      Serial.println (version);
+      Serial.println ();
+     Debg();
+   }
+  Serial.begin (9600);
+  mem_Read();                          //считываем данные в оперативку
+}
+
+
+void loop(){
+    
+      if (digitalRead(2) == 0){                                         //НАЖАТА  
+         if (millis()-time >= 1000){
+           LED =! LED;
+         }                   
+      }
+      
+      else {                                                            //ОТПУЩЕНА
+         if (millis()-time > 70 && millis()-time < 400 and LED == 1){         //изменение цвета       
+            num++; num %= 10;
+            delay (100);
+            //Serial.println (num);
+         }    
+         time = millis(); 
+      }
+      
+
+//---------------------LED-----------------------
+      
+      if (prev_LED==0 and LED==1){                       //включение
+        
+        while (a<=buff[num][3]){
+          analogWrite (R,buff[num][0]*a/256);
+          analogWrite (G,buff[num][1]*a/256);
+          analogWrite (B,buff[num][2]*a/256);
+          
+          a++;
+          delay (1);
+        }
+        prev_LED = LED;
+        delay (500);
+      
+      }
+      
+      if (prev_LED==1 and LED==1){                      //светит
+        if (buff[num][4]==0) mode_0(num);
+        if (buff[num][4]==1) mode_1(num);
+        if (buff[num][4]==2){randomSeed (millis()); mode_2(num);}
+        if (buff[num][4]==3){randomSeed (millis()); mode_3(num);}
+        if (buff[num][4]==4) mode_4(num);
+      }
+      
+      else if (prev_LED==1 and LED==0){                 //выключение
+        for (a=buff[num][3]; a >= 0; a--){                   
+          analogWrite (R,buff[num][0]*a/256);
+          analogWrite (G,buff[num][1]*a/256);
+          analogWrite (B,buff[num][2]*a/256);
+          delay (1);
+        }
+        prev_LED = LED;
+        delay (600);
+      }
+
+      else {                                            //не светит
+          analogWrite (R,0);
+          analogWrite (G,0);
+          analogWrite (B,0);
+      }
+      
+//-------------------LED---------------------
+ 
+
+  Serial.println (millis()-time);
+
+}      
+
+
+
+//------------------------------MODS-------------------------------
+
+void mode_0(int){                                          //просто свечение 
+     analogWrite (R,buff[num][0]*buff[num][3]/256.0); 
+     analogWrite (G,buff[num][1]*buff[num][3]/256.0);
+     analogWrite (B,buff[num][2]*buff[num][3]/256.0);  
+}
+
+
+void mode_1(int){                                          //плавное включение и выключение
+     
+     light = buff[num][3];
+     
+     if (up == 1) real_light++;
+     if (up == 0) real_light--;
+     
+     if (real_light > light)  up=0;
+     if (real_light < 0)      up=1;
+     
+     analogWrite (R,buff[num][0]*real_light/256.0); 
+     analogWrite (G,buff[num][1]*real_light/256.0);
+     analogWrite (B,buff[num][2]*real_light/256.0); 
+
+     delay (7);                        // <=== ЭТОГО ЗДЕСЬ БЫТЬ НЕДОЛЖНО !!!!!!!!!
+ 
+}
+
+
+void mode_2(int){                       //плавно от случайного к случайному
+     
+     if (prev_R_val == R_val&& prev_G_val == G_val&& prev_B_val == B_val){    //создание новых целей к которым нужно стремиться!!!
+       R_val = random(255); 
+       G_val = random(255);
+       B_val = random(255);
+     }
+     
+     if (prev_R_val < R_val)          //R
+       prev_R_val++;
+     else if (prev_R_val > R_val)
+       prev_R_val--;
+     
+     if (prev_G_val < G_val)          //G
+       prev_G_val++;
+     else if (prev_G_val > G_val)
+       prev_G_val--;
+       
+     if (prev_B_val < B_val)          //B
+       prev_B_val++;
+     else if (prev_B_val > B_val)
+       prev_B_val--;
+       
+
+     buff[num][0] = prev_R_val;
+     buff[num][1] = prev_G_val;
+     buff[num][2] = prev_B_val;
+     
+     analogWrite (R,prev_R_val); 
+     analogWrite (G,prev_G_val);
+     analogWrite (B,prev_B_val); 
+
+     /*Serial.println ("$");         //для проги (SerialPortPlotter.exe)
+     Serial.println (prev_R_val); 
+     Serial.println (" ");
+     Serial.println (prev_G_val); 
+     Serial.println (" ");
+     Serial.println (prev_B_val); 
+     Serial.println (";");*/
+     
+     delay (10);                   // <=== ЭТОГО ЗДЕСЬ БЫТЬ НЕДОЛЖНО !!!!!!!!!
+
+}
+
+
+
+void mode_3(int){
+       
+     if (real_light == 0){    //создание новых целей к которым нужно стремиться!!!
+       R_val = random(255); 
+       G_val = random(255);
+       B_val = random(255);
+     }
+     
+     light = buff[num][3];
+     buff[num][0] = R_val;
+     buff[num][1] = G_val;
+     buff[num][2] = B_val;
+     
+   
+     if (up == 1) real_light++;
+     if (up == 0) real_light--;
+     
+     if (real_light > light)  up=0;
+     if (real_light < 0)      up=1;
+     
+    
+     analogWrite (R,R_val*real_light/256.0); 
+     analogWrite (G,G_val*real_light/256.0);
+     analogWrite (B,B_val*real_light/256.0); 
+
+    /*
+     Serial.println ("$");         //для проги (SerialPortPlotter.exe)
+     Serial.println (R_val); 
+     Serial.println (" ");
+     Serial.println (G_val); 
+     Serial.println (" ");
+     Serial.println (B_val); 
+     Serial.println (";");
+    */ 
+     
+     delay (9);               // <=== ЭТОГО ЗДЕСЬ БЫТЬ НЕДОЛЖНО !!!!!!!!!
+
+}
+
+
+void mode_4(int){       //радуга
+ 
+}
+
+//------------------------------MODS-------------------------------
+
+
+
+
